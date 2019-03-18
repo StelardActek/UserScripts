def25d9de450965a409839a55056568c6f56e286
StelardActek <stelard@gmail.com>
2019-03-18 23:03:58 +1100

    Add files via upload

---
+++ Twitter Gallery Saveable.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Twitter Gallery Saveable
+// @namespace   http://steltechcor.com/
+// @include     https://twitter.com/*
+// @include     http://twitter.com/*
+// @version     1
+// @grant          GM_addStyle
+// ==/UserScript==
+
+(function() {
+  GM_addStyle('.GalleryNav--prev { width: 25% }');
+  GM_addStyle('.GalleryNav--next { width: 25% }');
+})();
