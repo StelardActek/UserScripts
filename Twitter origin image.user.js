ed55fe958fd95ee368cf182e58baa8231fde80ad
StelardActek <stelard@gmail.com>
2019-03-18 23:05:39 +1100

    I forget what I did to this one... maybe nothing?

---
+++ Twitter origin image.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         twitter origin image
+// @namespace    http://qqboxy.blogspot.com
+// @version      0.3
+// @description  jump to origin image link
+// @author       QQBoxy
+// @include      http://*pbs.twimg.com/media/*
+// @include      https://*pbs.twimg.com/media/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var is_orig = window.location.href.match(/\:orig$/);
+    if(!is_orig) {
+		var match = window.location.pathname.match(/^([^:]+)(\:?[^:]*)$/);
+    	window.location.href = window.location.origin + match[1] +":orig";
+    }
+})();
