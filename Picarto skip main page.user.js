03e04d49977816a31dc704038d85549b6eca40f5
StelardActek <stelard@gmail.com>
2019-03-18 23:17:42 +1100

    Update Picarto skip main page.user.js

--- Picarto skip main page.user.js
+++ Picarto skip main page.user.js
@@ -5,8 +5,7 @@
 // @description  Skips right to the explore page on picarto.tv
 // @author       Stelard Actek
 // @match        https://picarto.tv/
-// @downloadURL  https://dl.dropboxusercontent.com/u/2360939/GMscripts/Picarto%20skip%20main%20page.user.js?dl=1
-// @updateURL    https://dl.dropboxusercontent.com/u/2360939/GMscripts/Picarto%20skip%20main%20page.user.js?dl=1
+// @downloadURL  https://github.com/StelardActek/UserScripts/raw/master/Picarto%20skip%20main%20page.user.js
 // @grant        none
 // @run-at document-start
 // ==/UserScript==
