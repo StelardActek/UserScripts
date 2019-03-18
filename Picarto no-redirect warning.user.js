0b890a53e86523ee4595dc4973f4b1b544dfae4e
StelardActek <stelard@gmail.com>
2019-03-18 23:01:08 +1100

    Add files via upload

---
+++ Picarto no-redirect warning.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Picarto no-redirect warning
+// @namespace    http://stc.com/
+// @version      0.1
+// @description  Skip the 'you are leaving picarto' warning
+// @author       Stelard Actek
+// @match        https://picarto.tv/*
+// @downloadURL  https://www.dropbox.com/s/tezp2sss6tovtrk/Picarto%20no-redirect%20warning.user.js?raw=1
+// @updateURL    https://www.dropbox.com/s/tezp2sss6tovtrk/Picarto%20no-redirect%20warning.user.js?raw=1
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(() => {
+        //console.log('tick...');
+
+        let links = document.querySelectorAll('a[href*="/site/referrer"]');
+
+        for (let x = 0; x < links.length; x++) {
+            let link = links[x];
+            //console.log('found ', link.href, link);
+
+            let match = link.href.match(/[\?&]go=([^&]+)/);
+            if (match && match[1]) {
+                let newHref = decodeURIComponent(match[1]);
+                link.href = newHref;
+            }
+        }
+    }, 500);
+})();
