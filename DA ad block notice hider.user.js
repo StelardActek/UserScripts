9c8af6cd16d44fc620a866665cac6277243d3fa0
StelardActek <stelard@gmail.com>
2019-03-18 23:14:54 +1100

    Update DA ad block notice hider.user.js

--- DA ad block notice hider.user.js
+++ DA ad block notice hider.user.js
@@ -2,7 +2,8 @@
 // @name        DA ad block notice hider
 // @namespace   http://stc.com/
 // @include     http://*.deviantart.com/*
-// @version     1
+// @version     1.1
+// @downloadURL https://github.com/StelardActek/UserScripts/raw/master/DA%20ad%20block%20notice%20hider.user.js
 // @grant       GM_addStyle
 // @run-at      document-start
 // ==/UserScript==
