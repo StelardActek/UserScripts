d089967fee1016bb2b8b0c46d9513dec36133dc6
StelardActek <stelard@gmail.com>
2019-03-18 23:00:37 +1100

    Add files via upload

---
+++ Picarto Nice Emoticons.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         Picarto Nice Emoticons
+// @namespace    http://stc.com/
+// @version      1.3
+// @description  Replaces emoticon strings of your chosing with your custom emotes
+// @author       Stelard Actek
+// @match        https://picarto.tv/*
+// @downloadURL  https://www.dropbox.com/s/kxpu301xgidlwx8/Picarto%20Nice%20Emoticons.user.js?raw=1
+// @updateURL    https://www.dropbox.com/s/kxpu301xgidlwx8/Picarto%20Nice%20Emoticons.user.js?raw=1
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+
+
+// Settings - Edit these
+var emoticonMap = {
+    "^^/": ":cstelardactek-c1:",
+    ":0": ":cstelardactek-c2:",
+    ":o": ":cstelardactek-c2:",
+    ":box:": ":cstelardactek-c3:",
+    ":b": ":cstelardactek-c3:",
+    ":zzz:": ":cstelardactek-c4:",
+    ":z": ":cstelardactek-c4:",
+    ":peace:": ":cstelardactek-c5:",
+    ":y": ":cstelardactek-c5:"
+};
+
+var ignoreCase = true;
+
+
+
+
+// Code - Don't edit this, unless you know what you're doing
+$(document).ready(function () {
+    function escapeRegExp(str) {
+        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    }
+    
+    function replace(msg) {
+        //msg.val(msg.val().replace(":0", ":c2:"));
+        Object.keys(emoticonMap).forEach(function(key) {
+            var val = emoticonMap[key];
+            
+            var rex = new RegExp(escapeRegExp(key), ignoreCase ? 'ig' : 'g');
+            
+            msg.val(msg.val().replace(rex, " " + val + " "));
+        });
+    }
+    
+    var msg = $("input#msg");
+    if (msg && msg.length) {
+        msg.on("keydown", function(ev) {
+            if (ev.keyCode == 13) {
+                replace(msg);
+            }
+        });
+    }
+    
+    var send = $("input.sendbutton");
+    if (send && send.length) {
+        send.on("click", function(ev) {
+            replace(msg);
+        });
+        var evts = jQuery._data($("input.sendbutton")[0], "events").click;
+        evts.reverse();
+    }
+});
