01fd0c555b4ea02c6f88701b726cec2f496099f8
StelardActek <stelard@gmail.com>
2019-03-18 23:19:14 +1100

    Update Twitter Gallery Saveable.user.js

--- Twitter Gallery Saveable.user.js
+++ Twitter Gallery Saveable.user.js
@@ -4,7 +4,8 @@
 // @include     https://twitter.com/*
 // @include     http://twitter.com/*
 // @version     1
-// @grant          GM_addStyle
+// @downloadURL https://github.com/StelardActek/UserScripts/raw/master/Twitter%20Gallery%20Saveable.user.js
+// @grant       GM_addStyle
 // ==/UserScript==
 
 (function() {
